ca3b794767da729e48283c99d4d4f009747248db
ramanpreet0899 <preetraman905@gmail.com>
2021-09-15 11:51:36 -0400

    users list added

---
+++ KubraHomeAssignment/app/src/main/java/com/example/kubrahomeassignment/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.kubrahomeassignment
+
+import android.os.*
+import android.widget.*
+import androidx.appcompat.app.*
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val provider = UserProvider()
+        val service = provider.retrofit.create(UserService::class.java)
+
+        val call = service.getUsers()
+
+        call.enqueue(object : Callback<List<UserService.User>> {
+            override fun onResponse(
+                call: Call<List<UserService.User>>?,
+                response: Response<List<UserService.User>>?
+            ) {
+                response?.body()?.let{
+                    runOnUiThread {
+                        loadViews(it)
+                    }
+                }
+
+            }
+
+            override fun onFailure(call: Call<List<UserService.User>>?, t: Throwable?) {
+                Toast.makeText(
+                    applicationContext,
+                    t?.localizedMessage.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        })
+
+    }
+
+    private fun loadViews(users: List<UserService.User>) {
+        val adapter = UserAdapter(users)
+        recycler_view_users.adapter = adapter
+    }
+}
---
+++ KubraHomeAssignment/app/src/main/java/com/example/kubrahomeassignment/UserService.kt
@@ -0,0 +1,32 @@
+package com.example.kubrahomeassignment
+
+import retrofit2.*
+import retrofit2.http.*
+
+interface UserService {
+
+    data class User(
+        val id: Int,
+        val name: String,
+        val username : String,
+        val email : String,
+        val address: Address
+
+    )
+
+    data class Address(
+        val street:String,
+        val suite: String,
+        val city: String,
+        val zipcode: String,
+        val geo : Geo
+    )
+
+    data class Geo(
+        val lat: String,
+        val lng: String
+    )
+
+    @GET("users")
+    fun getUsers(): Call<List<User>>
+}
